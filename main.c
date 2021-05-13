5ec41dd4952ddb013e1d445b5970b9e322e770ef
kitamura-hub <masato1999824@icloud.com>
2021-05-13 15:04:57 +0900

    <Add>: 入金処理が完了しました

--- main.c
+++ main.c
@@ -26,18 +26,17 @@ typedef struct {
 // プロトタイプ宣言
 int mode(void);
 void show_product(product p_data[P_SIZE]);
+void payment(int *input);
 
 int main(void) {
   int total_sales = 0;  // 総売上
   int mode_number = 0;  // モードハンドリングの数値
-  // 総釣銭の初期化
-  total_change t_chnage_data = { 0, 0, 0, 0, 0 };
-  // 商品データの代入(初期化)
-  product product_data[P_SIZE] = {
+  int input_money = 0;  // 入金金額
+  total_change t_change_data = { 3, 5, 7, 9, 3 };  // 総釣銭
+  product product_data[P_SIZE] = {  // 商品データの代入(初期化)
     { 1, "水      ", 120, 3 }, { 2, "お茶    ", 130, 5 }, { 3, "紅茶    ", 110, 7 },
     { 4, "オレンジ", 100, 9 }, { 5, "コーラ  ", 110, 3 }, { 6, "ファンタ", 140, 5 }
   };
-
 
   // 商品の一覧表示
   show_product(product_data);
@@ -46,6 +45,10 @@ int main(void) {
   mode_number = mode();
   if (mode_number == 1) {
     puts("購入者モード");
+    payment(&input_money);  // 入金処理
+
+    // ②購入処理〜精算処理
+
   } else if (mode_number == 2) {
     puts("補充者モード");
   } else if (mode_number == 3) {
@@ -75,6 +78,22 @@ int mode() {
     scanf("%d", &mode_num);
     if (BETWEEN_0_TO_4) puts("有効な数値を入力してください！！");
   } while(BETWEEN_0_TO_4);
-
+  printf("\n");
   return mode_num;
+}
+
+void payment(int *input) {
+  int inp = 0;  // ユーザーの入力を受け取る変数
+  int y_n;  // y or n
+  puts("投入金額の上限は1990円まで");
+
+  do {
+    printf("投入金額を入力: ");
+    scanf("%d", &inp);
+    printf("投入金額は%d円です。こちらの金額で本当によろしいですか？(1=yes or 0=no): ", inp);
+    scanf("%d", &y_n);
+    if (inp > 1990 && y_n == 1) puts("投入金額の上限(1990円)を超えています");  // inpが1990円を超過　かつ　y_nが1のときに出力する
+  } while(y_n == 0 || inp > 1990);  // どちらかの条件が満たしていればループが行われる。両条件がfalseのときにループから抜ける
+
+  *input = inp;  // input_moneyの値を更新
 }
